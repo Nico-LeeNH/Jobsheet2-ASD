ea7194682ad9e3a8d1327c67dee27edc4c8f131e
Nico Lee <new.nikolee2007@gmail.com>
2026-02-20 09:40:15 +0700

    chore: add Mahasiswa object

--- Mahasiswa20.java
+++ Mahasiswa20.java
@@ -4,15 +4,17 @@ public class Mahasiswa20 {
     String kelas;
     double ipk;
 
-public Mahasiswa20(){
-    
-}
-public Mahasiswa20(String nm, String nim, double ipk, String kls){
-    nama = nm;
-    this.nim = nim;
-    this.ipk = ipk;
-    kelas = kls;
-}
+    public Mahasiswa20() {
+
+    }
+
+    public Mahasiswa20(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("Nim: " + nim);
@@ -20,16 +22,18 @@ public Mahasiswa20(String nm, String nim, double ipk, String kls){
         System.out.println("Kelas: " + kelas);
     }
 
-    void ubahKelas(String kelasBaru){
+    void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
     }
 
-    void updateIpk(double ipkBaru){
+    void updateIpk(double ipkBaru) {
         ipk = ipkBaru;
     }
 
-    String nilaiKinerja(){
-        if (ipk >= 3.5) {
+    String nilaiKinerja() {
+        if (ipk <= 0.0 || ipk >= 4.0) {
+            return "IPK tidak valid. Harus antara 0.0 dan 4.0";
+        } else if (ipk >= 3.5) {
             return "Kinerja sangat baik";
         } else if (ipk >= 3.0) {
             return "Kinerja Baik";
--- MahasiswaMain20.java
+++ MahasiswaMain20.java
@@ -11,5 +11,12 @@ public class MahasiswaMain20 {
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
 
+        Mahasiswa20 mhs2 = new Mahasiswa20("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+
+        Mahasiswa20 mhsNico = new Mahasiswa20("Nico Lee Nathanael Handoko",
+                "254107060005", 4.0, "SIB 1B");
+        mhsNico.tampilkanInformasi();
     }
 }
